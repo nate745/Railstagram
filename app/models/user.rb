008ddb2a4bcd209ce010e4f10cb6210aee42d85c
Nathan Alex <nate745@gmail.com>
2019-11-11 14:18:21 -0600

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :posts
     has_many :comments
     has_many :posts, through: :comments
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
 end
